3c5ceef15eb9fbe0c2e80115e8d66806c89af851
Sandro81 <sandro.fioravanti81@gmail.com>
2019-10-20 22:01:09 +0100

    https://github.com/Sandro81/SuperEnglishTestAngular.git

---
+++ src/app/app.routing.ts
@@ -0,0 +1,10 @@
+import { Routes } from "@angular/route";
+import {QuotesComponent} from './quotes/quotes.component';
+import {NewQuoteComponent} from './new-quote/new-quote.component';
+const APP_ROUTES: Routes = [
+  { path: '', component: QuotesComponent},
+  { path: new-quote',  component: NewQuoteComponent},
+]
+
+
+//https://www.youtube.com/watch?time_continue=22&v=g4GLfjcYhoQ
