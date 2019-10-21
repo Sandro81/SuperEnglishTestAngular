b5daabafaa5ae899e24e2965f08fcb3694a64a21
Sandro81 <sandro.fioravanti81@gmail.com>
2019-10-21 10:48:11 +0100

    https://github.com/Sandro81/SuperEnglishTestAngular.git

--- src/app/quote.service.ts
+++ src/app/quote.service.ts
@@ -1,18 +1,20 @@
 import {Injectable} from '@angular/core';
-import { Http, Response } from "@angular/http";
-import 'rxjs/Rx';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import { tap, map, filter } from 'rxjs/operators';
+
 
 @Injectable
 export class QuoteService {
-  constructor(private http: Http) {
+  constructor(private http: HttpClient) {
 
   }
 
-  getQuotes(){
-    return this.http.get('http://localhost/superenglishtest/public/api/quotes').map(
-      (response: Response) => {
-        return response.json().quotes;
-      }
-    );
+  getQuotes() {
+    return this.http.get('http://localhost/superenglishtest/public/api/quotes')
+      .map(
+        (response: Response) => {
+          return response.json().quotes;
+        }
+      );
   }
 }
